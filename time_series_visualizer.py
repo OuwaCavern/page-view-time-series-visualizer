22e0d22e2a9befdc41cd5fbef4a5d4d0dc825613
Raxzero <emircan.acikgoz@hotmail.com>
2024-03-07 11:59:20 +0300

    Add project files.

---
+++ time_series_visualizer.py
@@ -0,0 +1,56 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+from pandas.plotting import register_matplotlib_converters
+register_matplotlib_converters()
+
+# Import data (Make sure to parse dates. Consider setting index column to 'date'.)
+df = df = pd.read_csv("fcc-forum-pageviews.csv")
+df["date"] = pd.to_datetime(df["date"])
+df = df.set_index("date")
+
+# Clean data
+dfclean = df.drop(df[df["value"] > df["value"].quantile(0.975)].index)
+dfclean = df.drop(df[df["value"] < df["value"].quantile(0.025)].index)
+
+def draw_line_plot():
+    # Draw line plot
+
+
+
+
+
+    # Save image and return fig (don't change this part)
+    fig.savefig('line_plot.png')
+    return fig
+
+def draw_bar_plot():
+    # Copy and modify data for monthly bar plot
+    df_bar = None
+
+    # Draw bar plot
+
+
+
+
+
+    # Save image and return fig (don't change this part)
+    fig.savefig('bar_plot.png')
+    return fig
+
+def draw_box_plot():
+    # Prepare data for box plots (this part is done!)
+    df_box = df.copy()
+    df_box.reset_index(inplace=True)
+    df_box['year'] = [d.year for d in df_box.date]
+    df_box['month'] = [d.strftime('%b') for d in df_box.date]
+
+    # Draw box plots (using Seaborn)
+
+
+
+
+
+    # Save image and return fig (don't change this part)
+    fig.savefig('box_plot.png')
+    return fig
